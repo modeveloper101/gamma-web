8b772eefcee999270f9d4e1011bbf0c09b08afc5
Mohamed Rashwan <morashwan8@gmail.com>
2023-09-15 15:06:58 +0700

    update navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,30 @@
 import { FC, ReactElement } from "react";
 import Link from "next/link";
 import { bentham } from "@/app/layout";
-import { MapPinIcon, Bars2Icon } from "@heroicons/react/24/outline";
+import { MapPinIcon, Bars2Icon, PhoneIcon } from "@heroicons/react/24/outline";
 
 const Navbar: FC = (): ReactElement => {
   return (
     <header className="lg:pb-[10rem] container">
-      <div
-        className="relative h-32 bg-site-header"
-        style={{
-          backgroundPosition: "center center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-      <nav className="flex items-center justify-between py-[20px] px-[12px] lg:px-0 border-b border-neutral-200">
+      <div className="relative flex items-center justify-center h-11 bg-[#FAEBE2]">
+        <Link href="" className={`${bentham.className} text-xs uppercase`}>
+          Discover upcoming events
+        </Link>
+      </div>
+      <nav className="flex items-center justify-between py-[32px] lg:py-[48px] px-[12px] lg:px-0 border-b border-neutral-200">
         <Bars2Icon strokeWidth="0.8" className="block lg:hidden w-8 h-8" />
         <div className="hidden lg:flex items-center gap-x-[32px]">
           <Link
             href="/"
-            className="hidden lg:block text-sm tracking-wide uppercase text-neutral-700 hover:text-[#B9AC92] font-[400] duration-300"
+            className="hidden lg:block text-sm tracking-wide uppercase text-neutral-700 hover:text-[#DEB49C] font-[400] duration-300"
           >
             About
           </Link>
           <Link
             href="/"
-            className="hidden lg:block text-sm tracking-wide uppercase text-neutral-700 hover:text-[#B9AC92] font-[400] duration-300"
+            className="hidden lg:block text-sm tracking-wide uppercase text-neutral-700 hover:text-[#DEB49C] font-[400] duration-300"
           >
-            Events
+            Work
           </Link>
         </div>
         <Link
@@ -39,11 +36,11 @@ const Navbar: FC = (): ReactElement => {
         <div className="flex items-center gap-x-[16px]">
           <Link
             href="/"
-            className="hidden lg:block text-sm tracking-wide uppercase text-neutral-700 hover:text-[#B9AC92] font-[400] duration-300"
+            className="hidden lg:block text-sm tracking-wide uppercase text-neutral-700 hover:text-[#DEB49C] font-[400] duration-300"
           >
             Get in touch
           </Link>
-          <MapPinIcon strokeWidth="0.8" className="w-6 h-6" />
+          <PhoneIcon strokeWidth="0.8" className="w-5 h-5" />
         </div>
       </nav>
     </header>
